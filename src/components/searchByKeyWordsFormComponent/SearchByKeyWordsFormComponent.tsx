b064affa291aaef3e9271fb45235f6f114567977
Castor1983 <a0958086750@gmail.com>
2024-06-19 23:28:14 +0300

    css

--- src/components/searchByKeyWordsFormComponent/SearchByKeyWordsFormComponent.tsx
+++ src/components/searchByKeyWordsFormComponent/SearchByKeyWordsFormComponent.tsx
@@ -3,6 +3,7 @@ import {useForm} from 'react-hook-form'
 import { IKeyWordsModel } from '../../models/IKeyWordsModel';
 import { useAppDispatch } from '../../redux/store/store';
 import { movieActions } from '../../redux/slices/movieSlice';
+import css from './SearchByKeyWords.module.css'
 
 const SearchByKeyWordsFormComponent = () => {
     const {register,handleSubmit, reset} = useForm<IKeyWordsModel>();
@@ -18,8 +19,8 @@ const SearchByKeyWordsFormComponent = () => {
     return (
         <div>
             <form onSubmit={handleSubmit(search)}>
-                <input type='text' placeholder={'keyWords'} {...register('with_keywords')}/>
-                <button>Search</button>
+                <input  className={css.input} type='text' placeholder={'keyWords'} {...register('with_keywords')}/>
+                <button className={css.button}>Search</button>
             </form>
 
         </div>
